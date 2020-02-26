626a933cde7edf9e1d3d2d20a240eedb549814a5
Nicholas <nm9ur@virginia.edu>
2020-02-26 17:53:30 -0500

    kiss_sdl works

---
+++ src/kiss_sdl/DevTool.h
@@ -0,0 +1,32 @@
+#ifndef DEVTOOL_H
+#define DEVTOOL_H
+
+#include "AffineTransform.h"
+#include "AnimatedSprite.h"
+#include "Animations.h"
+#include "DisplayObjectContainer.h"
+#include "DisplayObject.h"
+#include "Game.h"
+#include "jsonConversions.h"
+#include "json.hpp"
+#include "Mouse.h"
+#include "Scene.h"
+#include "Sprite.h"
+#include "kiss_sdl.h"
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+using std::set;
+
+class DevTool : public Game{
+public:
+	DevTool();
+	virtual ~DevTool();
+	virtual void update(set<SDL_Scancode> pressedKeys);
+	virtual void draw(AffineTransform &at);
+private:
+	DisplayObject* draggable = NULL;
+};
+
+#endif
---
+++ src/kiss_sdl/Mouse.cpp
@@ -0,0 +1,116 @@
+#include "Mouse.h"
+#include "Game.h"
+#include <iostream>
+
+using namespace std;
+
+Mouse::Mouse() : DisplayObject(){
+ 	mouseCoords = {0,0};
+	leftClickCoords = {0,0};
+	leftClick = false;
+	rightClick = false;
+	isMoving = false;
+	wheelUp = false;
+	wheelDown = false;
+	isScrolling = false;
+	vis = false;
+}
+
+Mouse::Mouse(string id, int red, int green, int blue) : DisplayObject(id, red, green, blue){
+	mouseCoords = {0,0};
+	leftClickCoords = {0,0};
+	leftClick = false;
+	rightClick = false;
+	isMoving = false;
+	wheelUp = false;
+	wheelDown = false;
+	isScrolling = false;
+	vis = false;
+}
+
+Mouse::~Mouse(){
+
+}
+
+SDL_Point Mouse::getCoordinates(){
+	return this->mouseCoords;
+}
+
+void Mouse::setState(Uint32 eventType, const SDL_Event &event){
+	if (eventType == SDL_MOUSEBUTTONUP){
+		if (event.button.button == SDL_BUTTON_LEFT && this->leftClick) {
+	        this->leftClick = false;
+	        makeInvisible();
+	    }
+	    if(event.button.button == SDL_BUTTON_RIGHT){
+	    	this->rightClick = false;
+	    }
+
+	}
+	else if (eventType == SDL_MOUSEBUTTONDOWN){
+		if ( event.button.button == SDL_BUTTON_LEFT && !this->leftClick ) {
+	        this->leftClick = true;
+	        // get starting mouse coordinates
+	        SDL_GetMouseState( &this->leftClickCoords.x, &this->leftClickCoords.y );
+	        makeVisible();
+	    }
+
+	    if (event.button.button == SDL_BUTTON_RIGHT && !this->rightClick) {
+	        this->rightClick = true;
+	        // SDL_GetMouseState( &this->start_coords.x, &this->start_coords.y );
+    	}
+
+	} 
+
+	if(eventType == SDL_MOUSEWHEEL){
+		isScrolling = true;
+		if(event.wheel.y > 0 && !wheelUp)			wheelUp = true;
+		else if(event.wheel.y < 0 && !wheelDown)	wheelDown = true;
+
+	} else { 
+		wheelUp = false;
+		wheelDown = false;
+		isScrolling = false;
+	}
+
+	if ( eventType == SDL_MOUSEMOTION ) {
+        this->isMoving = true;
+        this->mouseCoords.x = event.motion.x;
+        this->mouseCoords.y = event.motion.y;
+    } else {
+        this->isMoving = false;
+    }
+
+}
+
+void Mouse::update(set<SDL_Scancode> pressedKeys){
+	DisplayObject::update(pressedKeys);
+
+}
+
+void Mouse::draw(AffineTransform &at){
+	if(!vis)	return;
+
+	w = this->mouseCoords.x - this->leftClickCoords.x;
+	h = this->mouseCoords.y - this->leftClickCoords.y;
+
+	SDL_Point tl = this->leftClickCoords;
+	SDL_Point pOrigin = {0,0};
+	if(w > 0 && h < 0){
+		h *= -1;
+		tl.y -= h; 
+	}
+	else if (w < 0 && h > 0){
+		w *= -1;
+		tl.x -= w;
+	}
+	else if (w < 0 && h < 0){
+		w *= -1; h *= -1;
+		tl.x -= w; tl.y -= h;
+	}
+
+	SDL_Rect dstrect = {tl.x, tl.y, w, h};
+
+	// cout << getCurTexture() << endl;
+	SDL_RenderCopyEx(Game::renderer, curTexture, NULL, &dstrect, 0, &pOrigin, SDL_FLIP_NONE);
+}
