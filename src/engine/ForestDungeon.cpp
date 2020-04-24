da26c5741826ade91112f037ced759b4baa59e41
dp7sv <dp7sv@virginia.edu>
2020-04-24 15:16:30 -0400

    added forest and olympus and cycling through them in dungeon manager

---
+++ src/engine/ForestDungeon.cpp
@@ -0,0 +1,27 @@
+#include "ForestDungeon.h"
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+ForestDungeon::ForestDungeon() {
+
+	id = "ForestDungeon";
+
+	basic_rooms_size = 5;
+	int basic_rooms0[] = { 0, 2, 3, 4, 5 };
+	basic_rooms = (int*) malloc(sizeof(int) * basic_rooms_size);
+	for (int i = 0; i < basic_rooms_size; i++) {
+		basic_rooms[i] = basic_rooms0[i];
+	}
+
+	scenes.push_back("./resources/scenes/ocean/basic_room.txt");
+	scenes.push_back("./resources/scenes/ocean/start.txt");
+	scenes.push_back("./resources/scenes/ocean/basic_room.txt");
+	scenes.push_back("./resources/scenes/ocean/basic_room.txt");
+	scenes.push_back("./resources/scenes/ocean/jelly_room.txt");
+	scenes.push_back("./resources/scenes/ocean/urchin_room.txt");
+}
+
+
+
---
+++ src/engine/OlympusDungeon.h
@@ -0,0 +1,19 @@
+#ifndef OLYMPUSDUNGEON_H
+#define OLYMPUSDUNGEON_H
+
+#include "Dungeon.h"
+
+using namespace std;
+
+class OlympusDungeon: public Dungeon {
+
+public:
+
+	OlympusDungeon();
+
+
+private:
+
+};
+
+#endif
