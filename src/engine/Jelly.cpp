f27043b6bdfcca2f7930b641d7cafd4e402ad593
dp7sv <dp7sv@virginia.edu>
2020-05-01 23:22:44 -0400

    made jellys go slower

--- src/engine/Jelly.cpp
+++ src/engine/Jelly.cpp
@@ -196,29 +196,29 @@ void Jelly::update(set<SDL_Scancode> pressedKeys) {
 			}
 		} //ySpe=-rand()%5;}
 	}
-	if(position.x>5 && position.x <xBound-5 && position.y>5 && position.y<yBound-5){
-	if (state == 0) {
-		timer++;
-		if (timer % 340 >= 250) {
-			state++;
-			xSpe = rand() % 3 - 1;
-			ySpe = rand() % 3 - 1;
-		}
-	} else if (state == 1) {
-		timer += 1;
-		if (timer % 340 >= 330) {
-			//timer = 0;
-			state = 0;
-			timer += 10 + rand() % 20;
-			while (abs(xSpe) + abs(ySpe) < 4 || abs(xSpe)+abs(ySpe)>11) {
-				xSpe = rand() % (9 + type) - 5;
-				ySpe = rand() % (9 + type) - 5;
+	if (position.x > 5 && position.x < xBound - 5 && position.y > 5
+			&& position.y < yBound - 5) {
+		if (state == 0) {
+			timer++;
+			if (timer % 340 >= 250) {
+				state++;
+				xSpe = rand() % 3 - 1;
+				ySpe = rand() % 3 - 1;
 			}
+		} else if (state == 1) {
+			timer += 1;
+			if (timer % 340 >= 330) {
+				//timer = 0;
+				state = 0;
+				timer += 10 + rand() % 20;
+				xSpe = rand() % (5 + 2 * type) - (2 + type);
+				ySpe = rand() % (5 + 2 * type) - (2 + type);
+			}
+		} else {
+			timer = 0;
+			state = 0;
 		}
-	} else {
-		timer = 0;
-		state = 0;
-	}}
+	}
 	if (timer < 0) {
 		timer = 0;
 	}
